ab6d2bfc1fd6ed8bf7fc28e5f0c0919b7d71a3d3
Alexander-susanin <72267284+Alexander-susanin@users.noreply.github.com>
2020-11-28 12:49:25 +0300

    Delete p2.py

--- 28.11.2020/p2.py
+++
@@ -1,6 +0,0 @@
-import itertools
-text = "Отихтрудовлишьяодинбездомный"
-part_1 = text[0:(len(text))//2]
-part_2 = text[len(text)//2:]
-res = ''.join(i + j for i, j in zip(part_1, part_2))
-print(res)
