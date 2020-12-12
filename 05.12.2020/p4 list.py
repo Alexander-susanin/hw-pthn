389a807e0e58ba69dcff81306574e79ab9e4823c
Alexander-susanin <72267284+Alexander-susanin@users.noreply.github.com>
2020-12-12 13:20:00 +0300

    Add files via upload

---
+++ 05.12.2020/p4 list.py
@@ -0,0 +1,25 @@
+import random
+
+n = int(input())
+a = [0 for i in range(n)]
+print(a)
+m = int(input())
+for i in range(m):
+    a[i] += 1
+for i in range(m,n):
+        a[i] += 2
+print(a)
+       
+for i in range(n):
+    if (len(a) == 1):
+        break
+    try: 
+        a[m] += a[m - 1]
+        a.pop(m - 1)
+    except IndexError:    
+         a[-1] += a[0]
+         a.pop(0)
+        
+    
+print ("The last one:", m - 1)
+print ("Total coins:", a[0])
